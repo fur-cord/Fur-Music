044020f4a07c35b83f66392d1b8e7825fc2c7e2c
CatGirlUmi <288700637+CatGirlUmi@users.noreply.github.com>
2026-08-31 19:39:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -251,7 +251,7 @@ def update_rpc():
             }
 
         activity['buttons'] = [
-            {"label": "View on GitHub", "url": "https://github.com/your-username/your-repo"}
+            {"label": "View on GitHub", "url": "https://github.com/katt-dev/Katt-Music"}
         ]
 
         discord_ipc.set_activity(activity)
